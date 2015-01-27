74b07615e1ad9e52919b2f65eaf282d08490f9bd
Constantin Pralea <constantin.pralea@endava.com>
2015-01-27 11:52:08 +0200

    Removed credentials from code.

--- src/main/com/endava/cloudpractice/instantvm/Configuration.java
+++ src/main/com/endava/cloudpractice/instantvm/Configuration.java
@@ -3,8 +3,8 @@ package com.endava.cloudpractice.instantvm;
 
 public class Configuration {
 
-	public static final String AWS_ACCESS_KEY_ID = "AKIAJ6STHOUVHT7R2KWQ";
-	public static final String AWS_SECRET_KEY = "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";
+	public static final String AWS_ACCESS_KEY_ID = "<rotated>";
+	public static final String AWS_SECRET_KEY = "<rotated>";
 
 	public static final String AWS_DDB_ENDPOINT = "dynamodb.us-west-2.amazonaws.com";
 
