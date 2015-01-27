c727f9207e9a096347983ab0f76af4e1a64ff034
Constantin Pralea <constantin.pralea@endava.com>
2015-01-27 12:14:04 +0200

    Reading credentials from environment variables.

--- src/main/com/endava/cloudpractice/instantvm/Configuration.java
+++ src/main/com/endava/cloudpractice/instantvm/Configuration.java
@@ -1,11 +1,26 @@
 package com.endava.cloudpractice.instantvm;
 
+import java.util.Map;
+
 
 public class Configuration {
 
-	public static final String AWS_ACCESS_KEY_ID = "<rotated>";
-	public static final String AWS_SECRET_KEY = "<rotated>";
+	public static final String AWS_ACCESS_KEY_ID;
+	public static final String AWS_SECRET_KEY;
 
 	public static final String AWS_DDB_ENDPOINT = "dynamodb.us-west-2.amazonaws.com";
 
+	static {
+		Map<String, String> env = System.getenv();
+		AWS_ACCESS_KEY_ID = env.get("AWS_ACCESS_KEY_ID");
+		AWS_SECRET_KEY = env.get("AWS_SECRET_KEY");
+		
+		if(AWS_ACCESS_KEY_ID == null || AWS_ACCESS_KEY_ID.isEmpty()) {
+			throw new RuntimeException("AWS_ACCESS_KEY_ID environment variable is not set.");
+		}
+		if(AWS_SECRET_KEY == null || AWS_SECRET_KEY.isEmpty()) {
+			throw new RuntimeException("AWS_SECRET_KEY environment variable is not set.");
+		}
 	}
+
+}
