87e703904057e2eb333a4173a3d4bab81f48a243
Constantin Pralea <constantin.pralea@endava.com>
2015-02-04 15:58:04 +0200

    Added Mockito as the mocking framework of choice.

--- src/test/com/endava/cloudpractice/instantvm/orchestrator/BasicOrchestratorTest.java
+++ src/test/com/endava/cloudpractice/instantvm/orchestrator/BasicOrchestratorTest.java
@@ -1,9 +1,31 @@
 package com.endava.cloudpractice.instantvm.orchestrator;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import com.endava.cloudpractice.instantvm.instances.VMManager;
+import com.endava.cloudpractice.instantvm.repository.VMDefinitionRepository;
 
 public class BasicOrchestratorTest {
+
+	private BasicOrchestrator orchestrator;
+
+
+	@Before
+	public void before() {
+		VMDefinitionRepository defRepository = Mockito.mock(VMDefinitionRepository.class);
+		VMManager vmManager = Mockito.mock(VMManager.class);
+		orchestrator = new BasicOrchestrator(defRepository, vmManager);
+	}
+
+
+	@After
+	public void after() {
+		orchestrator = null;
+	}
+
 
 	@Test
 	public void dummy() {
